d86d6a20f6e8278ed34c0daf7b0470c64383a4fc
HoangHai <hoangminhhai1997@gmail.com>
2018-06-27 23:41:18 +0700

    new commit from master

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,6 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Heluuu World!!!");
+        System.out.println("Heluuu World!!!222222");
     }
 }
